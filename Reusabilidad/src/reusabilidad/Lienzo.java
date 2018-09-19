d24a3102470d3230b4df4b8b850b1991fd564f57
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-09-18 20:13:45 -0500

    Agrega funcion de sector y corrige errores

--- Reusabilidad/src/reusabilidad/Lienzo.java
+++ Reusabilidad/src/reusabilidad/Lienzo.java
@@ -46,10 +46,14 @@ public class Lienzo {
      */
     public void dibujaLinea(int x1, int y1, int x2, int y2, char c)
     {
-        // Ordena puntos
+        // Ordena automáticamente las coordenadas dadas, a fin
+        // de que p1 sea aquel punto que se encuentre más cercano a 0.
+        // Si la función es llamada desde la sobrecarga de esta funcion
+        // donde se pide una distancia y un ángulo, en caso que el ángulo
+        // sea igual a 270°, también reorganizará los puntos.
         int tx = x1;
         int ty = y1;
-        if(x1 > x2)
+        if(x1 > x2 || (x1 == x2 && y2 < y1))
         {
             x1 = x2;
             y1 = y2;
@@ -75,12 +79,22 @@ public class Lienzo {
         }
     }
     
-    public void dibujaLinea(int x1, int y1, double d, double t, char c)
+    /**
+     * Dibuja una línea dado un punto inicial, la distancia y el ángulo
+     * @param x1 Coordenada X
+     * @param y1 Coordenada Y
+     * @param d Distancia de la línea
+     * @param t Ángulo con respecto al Y
+     * @param c Caracter que será usado para dibujar el círculo.
+     */
+    public int[] dibujaLinea(int x1, int y1, double d, double t, char c)
     {
-        int x = (int)(Math.cos(t) * d);
-        int y = (int)(Math.sin(t) * d);
+        int x = (int)(Math.cos(Math.toRadians(t)) * d);
+        int y = (int)(Math.sin(Math.toRadians(t)) * d);
+        int[] rv = {x, y};
         
-        dibujaLinea(x1, y1, x1 + x, y1 + y, c);        
+        dibujaLinea(x1, y1, x1 + x, y1 + y, c);
+        return rv;
     }
 
     /**
@@ -94,20 +108,8 @@ public class Lienzo {
     {
         for(double t = 0; t <= Math.PI*2; t += 0.1)
         {
-            double x = x1 + r * Math.cos(t);
-            double y = y1 + r * Math.sin(t);
-
-            if((int)x < 0 || (int)y < 0 || (int)x > this.ancho || (int)y > this.alto)
-            {
-                System.out.println("Error: El círculo sale de los límites del lienzo. "
-                        + "\nReplantee los puntos. \n\nInformación extra:");
-                System.out.println("x:\t" + (int)x + "\ny:\t" + (int)y + 
-                        "\n\nLienzo\nAlto:\t" + this.getAlto() +
-                        "\nAncho:\t" + this.getAncho());
-                
-                break;
-            } else
-                this.setPen((int)x, (int)y, c);
+            int rv = auxDibujaCirculo(x1, y1, r, t, c);
+            if(rv == 0) break;
         }
     }
     
@@ -131,10 +133,44 @@ public class Lienzo {
         
         dibujaCirculo(xc, yc, r, c);
     }
-    
-    public void dibujaSector()
-    {
         
+    private int auxDibujaCirculo(int x1, int y1, int r, double t, char c)
+    {
+        double x = x1 + r * Math.cos(t);
+        double y = y1 + r * Math.sin(t);
+
+        if((int)x < 0 || (int)y < 0 || (int)x > this.ancho || (int)y > this.alto)
+        {
+            System.out.println("Error: El círculo sale de los límites del lienzo. "
+                    + "\nReplantee los puntos. \n\nInformación extra:");
+            System.out.println("x:\t" + (int)x + "\ny:\t" + (int)y + 
+                    "\n\nLienzo\nAlto:\t" + this.getAlto() +
+                    "\nAncho:\t" + this.getAncho());
+
+            return 0;
+        } else
+        {
+            this.setPen((int)x, (int)y, c);
+            return 1;
+        }
+    }
+    
+    /**
+     * Dibuja sector de un círculo en base a un radio, un ángulo de inicio y
+     * un ángulo "de apertura"
+     * @param x1 Coordenada X del punto
+     * @param y1 Coordenada Y del punto
+     * @param r Radio
+     * @param t1 Ángulo de inicio
+     * @param t2 Ángulo de fin
+     * @param c Caracter que será usado para dibujar el círculo.
+     */
+    public void dibujaSector(int x1, int y1, int r, double t1, double t2, char c)
+    {
+        this.dibujaLinea(x1, y1, r, t1, c);
+        this.dibujaLinea(x1, y1, r, t2, c);
+        
+        this.dibujaCirculo(x1, y1, r, c);
     }
     
     /**
@@ -156,7 +192,7 @@ public class Lienzo {
         for(int y = this.alto - 1; y >= 0; y--) {
             System.out.println();
             for(int x = 0; x < this.ancho; x++) {
-                System.out.print(this.lienzo[x][y]);
+                System.out.print(this.lienzo[x][y] + " ");
             }
         }
         System.out.println();
--- Reusabilidad/src/reusabilidad/Reusabilidad.java
+++ Reusabilidad/src/reusabilidad/Reusabilidad.java
@@ -32,13 +32,26 @@ public class Reusabilidad {
         // terreno.paint();
         
         // Tarea
-        /* Lienzo lm = new Lienzo();
+        Lienzo lm = new Lienzo();
+        System.out.println("\n\nDibuja Círculo");
+        System.out.println("Dibuja círculo que pase por dos puntos que serán " +
+                "tomados como los puntos del diametro del círculo.");
         lm.dibujaCirculo(30, 40, 80, 80, '*');
-        lm.paint(); */
+        lm.paint();
         
         Lienzo lin = new Lienzo();
-        lin.dibujaLinea(50, 20, 10d, 300d, '*');
+        System.out.println("\n\nDibuja línea");
+        System.out.println("Dibuja una línea en base a su punto inicial, su "
+                + "distancia y el ángulo que tiene en relación al eje X.");
+        lin.dibujaLinea(50, 0, 30d, 160d, '*');
         lin.paint();
+        
+        Lienzo lsec = new Lienzo();
+        System.out.println("\n\nDibuja sector");
+        System.out.println("Dibuja un círculo y un sector de éste en base"
+                + " a los ángulos dados en los parámetros");
+        lsec.dibujaSector(20,0, 20, 90d, 135d, 'r');
+        lsec.paint();
     }
     
 }
